bb23ec70404de2c4e1130dac3ee45d5eec9eb075
infirit <infirit@gmail.com>
2015-07-23 22:54:36 +0200

    MainWindow: Fix drag/drop
    Based on alacarte commits
    
    0f6a88025f13e02433f004bb88c1daefb6f18eb1 Lars Kruse <devel@sumpfralle.de>
    6dc0fa695cbf16bc6d4f7daadec4b2f1c4f7c66b Jasper St. Pierre <jstpierre@mecheye.net>

--- Mozo/MainWindow.py
+++ Mozo/MainWindow.py
@@ -396,9 +396,9 @@ class MainWindow:
 	def on_menu_tree_drag_data_received(self, treeview, context, x, y, selection, info, etime):
 		menus = treeview.get_model()
 		drop_info = treeview.get_dest_row_at_pos(x, y)
+		types = (gtk.TREE_VIEW_DROP_INTO_OR_BEFORE, gtk.TREE_VIEW_DROP_INTO_OR_AFTER)
 		if drop_info:
 			path, position = drop_info
-			types = (gtk.TREE_VIEW_DROP_INTO_OR_BEFORE, gtk.TREE_VIEW_DROP_INTO_OR_AFTER)
 			if position not in types:
 				context.finish(False, False, etime)
 				return False
@@ -413,6 +413,8 @@ class MainWindow:
 				elif item.get_type() == matemenu.TYPE_DIRECTORY:
 					if self.editor.moveMenu(item, new_parent) == False:
 						self.loadUpdates()
+				elif item.get_type() == matemenu.TYPE_SEPARATOR:
+					self.editor.moveSeparator(item, new_parent)
 				else:
 					context.finish(False, False, etime) 
 				context.finish(True, True, etime)
@@ -489,6 +491,9 @@ class MainWindow:
 
 	def on_item_tree_drag_data_received(self, treeview, context, x, y, selection, info, etime):
 		items = treeview.get_model()
+		types_into = (gtk.TREE_VIEW_DROP_INTO_OR_BEFORE, gtk.TREE_VIEW_DROP_INTO_OR_AFTER)
+		types_before = (gtk.TREE_VIEW_DROP_BEFORE, gtk.TREE_VIEW_DROP_INTO_OR_BEFORE)
+		types_after = (gtk.TREE_VIEW_DROP_AFTER, gtk.TREE_VIEW_DROP_INTO_OR_AFTER)
 		types = (gtk.TREE_VIEW_DROP_BEFORE,	gtk.TREE_VIEW_DROP_INTO_OR_BEFORE)
 		if selection.target == 'MOZO_ITEM_ROW':
 			drop_info = treeview.get_dest_row_at_pos(x, y)
@@ -497,22 +502,32 @@ class MainWindow:
 			if self.drag_data == None:
 				return False
 			item = self.drag_data
+			# by default we assume, that the items stays in the same menu
+			destination = item.get_parent()
 			if drop_info:
 				path, position = drop_info
-				if position in types:
-					before = items[path][3]
+				target = items[path][3]
+				# move the item to the directory, if the item was dropped into it
+				if (target.get_type() == matemenu.TYPE_DIRECTORY) and (position in types_into):
+					# append the selected item to the choosen menu
+					destination = target
+				elif position in types_before:
+					before = target
+				elif position in types_after:
+					after = target
 				else:
-					after = items[path][3]
+					# this does not happen
+					pass
 			else:
 				path = (len(items) - 1,)
 				after = items[path][3]
 			if item.get_type() == matemenu.TYPE_ENTRY:
-				self.editor.moveItem(item, item.get_parent(), before, after)
+				self.editor.moveItem(item, destination, before, after)
 			elif item.get_type() == matemenu.TYPE_DIRECTORY:
-				if self.editor.moveMenu(item, item.get_parent(), before, after) == False:
+				if self.editor.moveMenu(item, destination, before, after) == False:
 					self.loadUpdates()
 			elif item.get_type() == matemenu.TYPE_SEPARATOR:
-				self.editor.moveSeparator(item, item.get_parent(), before, after)
+				self.editor.moveSeparator(item, destination, before, after)
 			context.finish(True, True, etime)
 		elif selection.target == 'text/plain':
 			if selection.data == None:
@@ -524,7 +539,7 @@ class MainWindow:
 			after = None
 			if drop_info:
 				path, position = drop_info
-				if position in types:
+				if position in types_before:
 					before = items[path][3]
 				else:
 					after = items[path][3]

b24d840b596abd535c7fa6148c98d42b3d8485e3
infirit <infirit@gmail.com>
2015-07-24 12:09:07 +0200

    Add properties and a delete button

--- Mozo/MainWindow.py
+++ Mozo/MainWindow.py
@@ -388,6 +388,8 @@ class MainWindow:
 		self.tree.get_object('move_up_button').set_sensitive(False)
 		self.tree.get_object('move_down_button').set_sensitive(False)
 		self.tree.get_object('new_separator_button').set_sensitive(False)
+		self.tree.get_object('properties_button').set_sensitive(False)
+		self.tree.get_object('delete_button').set_sensitive(False)
 
 	def on_menu_tree_drag_data_get(self, treeview, context, selection, target_id, etime):
 		menus, iter = treeview.get_selection().get_selected()
@@ -437,14 +439,17 @@ class MainWindow:
 		item = items[iter][3]
 		self.tree.get_object('edit_delete').set_sensitive(True)
 		self.tree.get_object('new_separator_button').set_sensitive(True)
+		self.tree.get_object('delete_button').set_sensitive(True)
 		if self.editor.canRevert(item):
 			self.tree.get_object('edit_revert_to_original').set_sensitive(True)
 		else:
 			self.tree.get_object('edit_revert_to_original').set_sensitive(False)
 		if not item.get_type() == matemenu.TYPE_SEPARATOR:
 			self.tree.get_object('edit_properties').set_sensitive(True)
+			self.tree.get_object('properties_button').set_sensitive(True)
 		else:
 			self.tree.get_object('edit_properties').set_sensitive(False)
+			self.tree.get_object('properties_button').set_sensitive(False)
 
 		# If first item...
 		if items.get_path(iter)[0] == 0:
@@ -622,6 +627,11 @@ class MainWindow:
 
 	def on_close_button_clicked(self, button):
 		self.tree.get_object('mainwindow').hide()
+
+	def on_properties_button_clicked(self, button):
+		self.on_edit_properties_activate(None)
+	def on_delete_button_clicked(self, button):
+		self.on_edit_delete_activate(None)
 
 	def on_style_set(self, *args):
 		self.loadUpdates()
